81b5859c382422dfb9cc55e090753773934844cc
Vifleem <sobole_80@mail.ru>
2020-03-07 00:27:54 +0600

    Updates

---
+++ precache-manifest.02fd854728852573aaa07639bac6db5a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12e0b785476c65d87cd494efb4f830b5",
+    "url": "./index.html"
+  },
+  {
+    "revision": "8cec0058a39a9254d241",
+    "url": "./static/css/2.3e9c613c.chunk.css"
+  },
+  {
+    "revision": "d1acc9cbf72d0d44544e",
+    "url": "./static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "8cec0058a39a9254d241",
+    "url": "./static/js/2.da47d69b.chunk.js"
+  },
+  {
+    "revision": "21ced859ea2b2d6b856d461ad6c2afed",
+    "url": "./static/js/2.da47d69b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1acc9cbf72d0d44544e",
+    "url": "./static/js/main.9fd317c3.chunk.js"
+  },
+  {
+    "revision": "924f1ed2fcb4d5f13259",
+    "url": "./static/js/runtime-main.4f8e16d7.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "./static/media/persik.4e1ec840.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ae0f932bfb2bc62a3f69539999627ca2.js"
+  "./precache-manifest.02fd854728852573aaa07639bac6db5a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
